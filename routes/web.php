bc28c22f0ee0202e0fc9d32cf918e1dfa52820a9
guicosme1 <gui.c.p.dal@gmail.com>
2021-09-03 21:24:43 -0300

    Segundo commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('movie', MovieController::class);
+
+
