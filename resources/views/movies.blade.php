ab8a88462a72bd2e3d6e9ffb0cba3199b7efb6d1
guicosme1 <gui.c.p.dal@gmail.com>
2021-09-05 16:32:10 -0300

    terceiro commit

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,11 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies | Adapti</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 </head>
+<style>
+    body {
+        background: rgb(29, 29, 29);
+        font-family: 'Roboto', sans-serif;
+        color: white;
+    }
+</style>
 <body>
     @foreach ($movies as $movie)
-        <h4>{{ $movie->title }}</h4>
-        <img src="{{ $movie->image }}" alt="Imagem">
+        <h4>{{ $movie->title }} from {{ $movie->country->pais }}</h4>
+        
     @endforeach
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('movie', MovieController::class);
-
-
